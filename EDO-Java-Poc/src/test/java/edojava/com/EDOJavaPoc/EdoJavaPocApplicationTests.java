3571cc5e4419ec42660f86de7de05d53ceb5d5de
ahamedzubair0207 <74019426+ahamedzubair0207@users.noreply.github.com>
2022-05-10 21:18:14 -0500

    Initial Commit

---
+++ EDO-Java-Poc/src/test/java/edojava/com/EDOJavaPoc/EdoJavaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package edojava.com.EDOJavaPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdoJavaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
